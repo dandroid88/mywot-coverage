35694c1c3d71dec6458040d95da8479f5d4f3ea7
Daniel Myers <DanielMyers2007@u.northwestern.edu>
2012-04-09 13:28:56 -0500

    added beautiful soup which we will use later for parsing {maybe}

--- checkWOT.py
+++ checkWOT.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-import sys, urllib2, bs4, re
+import sys, urllib2, re
+from bs4 import BeautifulSoup
 if (len(sys.argv) > 1):
     url = sys.argv[1]
     if not re.match('(?=http)\w+', url):
         print "Please provide a valid URL"
     else:
         print "Loading " + url
-        page = urllib2.urlopen(url)
-        print page.read()
+        page = urllib2.urlopen(url).read()
+        print BeautifulSoup(page).prettify()
