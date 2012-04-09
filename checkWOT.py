bf6dedfc2823f7224a043ef255d54390b8a8b8bc
Daniel Myers <DanielMyers2007@u.northwestern.edu>
2012-04-09 13:39:59 -0500

    added a mywot api call demo

--- checkWOT.py
+++ checkWOT.py
@@ -6,6 +6,8 @@ if (len(sys.argv) > 1):
     if not re.match('(?=http)\w+', url):
         print "Please provide a valid URL"
     else:
+        apiResponse = urllib2.urlopen('http://api.mywot.com/0.4/public_query2?url=' + url).read()
+        print apiResponse
         print "Loading " + url
         page = urllib2.urlopen(url).read()
-        print BeautifulSoup(page).prettify()
+        #print BeautifulSoup(page).prettify()
