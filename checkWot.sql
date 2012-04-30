0b293684bbd4b77833fa39482c5e28ac24087ab8
Daniel Myers <DanielMyers2007@u.northwestern.edu>
2012-04-29 21:45:57 -0500

    updated schema

--- checkWot.sql
+++ checkWot.sql
@@ -1,15 +1,13 @@
 CREATE TABLE IF NOT EXISTS chains
 (
-    chain_id INT NOT NULL,
-	PRIMARY KEY (chain_id)
+    chain_id INT AUTO_INCREMENT PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS urls
 (
-	url_id INT NOT NULL,
+	url_id INT AUTO_INCREMENT PRIMARY KEY,
 	url VARCHAR(1024), 
     
-    --Official Scores
 	Trustworthiness INT, 
 	Trust_confidence INT, 
 	Vendor_Reliability INT, 
@@ -19,14 +17,11 @@ CREATE TABLE IF NOT EXISTS urls
 	Child_safety INT, 
 	Child_confidence INT, 
 
-    --Used for the URLs.txt File with format:
-    --  http://bit.ly/jU0kgr nonspam 1 1309265860 1309265860 
     spam INT,
     occurances INT,    
     time_1 INT,
     time_2 INT,
 
-    --Comment Statistics
     good_site INT,
     useful_informative INT,
     entertaining INT,
@@ -36,13 +31,12 @@ CREATE TABLE IF NOT EXISTS urls
     adult_content INT,
     other INT,
 
-	PRIMARY KEY (url_id),
 	FOREIGN KEY (url_id) REFERENCES chains(chain_id)
 );
 
 CREATE TABLE IF NOT EXISTS comments
 (
-	comment_id INT NOT NULL,
+	comment_id INT AUTO_INCREMENT PRIMARY KEY,
     comment_date date,
     author VARCHAR(100),
 	text VARCHAR(64000),
@@ -51,7 +45,6 @@ CREATE TABLE IF NOT EXISTS comments
     votesDisabled INT,
     upvotes INT,
     downvotes INT,
-	PRIMARY KEY (comment_id),
 	FOREIGN KEY (comment_id) REFERENCES urls(url_id)
 );
 
